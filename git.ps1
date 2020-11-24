5841b7cd33b1968a9f42e3d46de9004a939b5fb9
William Barbosa <heytherewill@gmail.com>
2020-11-24 18:54:14 +0100

    :octocat: Add git script

---
+++ git.ps1
@@ -0,0 +1,21 @@
+Set-Alias gaa 'git add -A'
+
+function gcm($message) {
+    git commit -m $message
+}
+function gca($message) {
+    git add -A
+    git commit -m $message
+}
+
+function gwip() {
+    gca(":construction: WIP")
+}
+
+function gri($size) {
+    git rebase -i HEAD~$size
+}
+
+function grh($size) {
+    git reset HEAD~$size
+}
