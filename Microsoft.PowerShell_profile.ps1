fd23063c3fa71d35eff1b4e42a156a7c9a78340e
William Barbosa <heytherewill@gmail.com>
2020-11-25 11:01:25 +0100

    :book: Document stuff I will forget eventually

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,25 @@
+# My aliases and custom scripts
 $root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . "$root/android.ps1"
 . "$root/git.ps1"
 . "$root/terminal.ps1"
 
+# Package Manager
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 
+# Git support
 Import-Module posh-git
+
+# Command line editing
 Import-Module PSReadLine
+
+# z to jump around
 Import-Module ZLocation
+
+# Prompt
 Invoke-Expression (&starship init powershell)
 
 # Fish-like autocompletion
