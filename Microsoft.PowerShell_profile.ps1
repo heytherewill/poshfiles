67dd29b51970a3936adfb03f58bf30fb888120bd
William Barbosa <heytherewill@gmail.com>
2020-11-25 10:41:44 +0100

    :file_folder: Add terminal script file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
 $root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-
 . "$root/android.ps1"
 . "$root/git.ps1"
+. "$root/terminal.ps1"
+Clear-Host
--- git.ps1
+++ git.ps1
@@ -3,6 +3,7 @@ Set-Alias gaa 'git add -A'
 function gcm($message) {
     git commit -m $message
 }
+
 function gca($message) {
     git add -A
     git commit -m $message
