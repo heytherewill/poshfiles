f56de43b336c7d1e9a609eadd3309bf8e9ea3ee9
William Barbosa <heytherewill@gmail.com>
2020-11-25 10:42:18 +0100

    :chocolate_bar: Add Chocolatey

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,4 +2,9 @@ $root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . "$root/android.ps1"
 . "$root/git.ps1"
 . "$root/terminal.ps1"
+
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 Clear-Host
