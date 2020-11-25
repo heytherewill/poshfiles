176da4f0e9c90d266d9a4db45b1023692425a030
William Barbosa <heytherewill@gmail.com>
2020-11-25 10:48:18 +0100

    :tophat: Add posh-git

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,4 +7,6 @@ $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
+
+Import-Module posh-git
 Clear-Host
---
+++ modules.ps1
@@ -0,0 +1,7 @@
+function Get-Module-Not-Installed($module) {
+    return -not(Get-InstalledModule $module -ErrorAction silentlycontinue)
+}
+
+if (Get-Module-Not-Installed posh-git) {
+    Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
+}
