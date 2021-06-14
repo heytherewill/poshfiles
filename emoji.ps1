58a71e1b103e1fdaa151302110232773d092dc94
William Barbosa <heytherewill@gmail.com>
2021-06-14 07:33:39 +0200

    💪 Even better emoji autocompletion

--- emoji.ps1
+++ emoji.ps1
@@ -5,7 +5,17 @@ if ((Test-Path $emojiFile) -eq $false) {
   Invoke-RestMethod "https://raw.githubusercontent.com/github/gemoji/master/db/emoji.json" | ConvertTo-Json | Out-File -FilePath $emojiFile
 }
 
-$emojiList = Get-Content $emojiFile | ConvertFrom-Json | ForEach-Object { $_.aliases }
+$emojiList = @()
+$emojiTable = @{}
+$rawEmojiData = Get-Content $emojiFile | ConvertFrom-Json
+
+$emojiList = $rawEmojiData | ForEach-Object { $_.aliases }
+$emojiTable = @{}
+foreach ($emojiInfo in $rawEmojiData) {
+    foreach($alias in $emojiInfo.aliases) {
+        $emojiTable[$alias] = $emojiInfo.emoji
+    }
+}
 
 # Set autocompletion
 Set-PSReadLineKeyHandler -Chord 'Shift+:' `
@@ -19,7 +29,5 @@ Set-PSReadLineKeyHandler -Chord 'Shift+:' `
     [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
     $pickedEmoji = $emojiList | fzf
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($pickedEmoji)
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar) ")
+    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($emojiTable[$pickedEmoji])
 }
