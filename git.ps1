5360d98d269008afbab91c52b169b36472d958aa
William Barbosa <heytherewill@gmail.com>
2021-06-14 06:36:27 +0200

    :deciduous_tree: Better log

--- git.ps1
+++ git.ps1
@@ -29,6 +29,10 @@ function gcma($message) {
     git commit -m $message
 }
 
+function glog() {
+    git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+}
+
 function gnuke() {
     gaa
     git reset --hard HEAD 
