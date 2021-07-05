2415173c7b278c4cc56f1975c5affb49ccad8b44
William Barbosa <heytherewill@gmail.com>
2021-07-05 10:02:33 +0200

    🤕 Add typo list

--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,7 @@ $root = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 . "$root/android.ps1"
 . "$root/git.ps1"
 . "$root/terminal.ps1"
+. "$root/typo.ps1"
 . "$root/rg.ps1"
 
 if ($IsWindows) {
---
+++ typo.ps1
@@ -0,0 +1,14 @@
+function cargp { 
+    Write-Output "cargo*"
+    cargo $args 
+}
+
+function got { 
+    Write-Output "git*"
+    git $args
+}
+
+function gut { 
+    Write-Output "git*"
+    git $args
+}
