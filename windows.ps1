706e01b1832cac06ba12f55d5dad88dece24da1e
William Barbosa <heytherewill@gmail.com>
2021-07-08 18:18:29 +0200

    📃 Add function to copy current path

--- windows.ps1
+++ windows.ps1
@@ -4,10 +4,19 @@ function bin() {
     open shell:RecycleBinFolder
 }
 
-function touch($fileName) {
-    fsutil file createnew $fileName 0
+function ccd() {
+    $prefix = "        Directory: "
+    
+    Get-ChildItem 
+    | rg $prefix
+    | ForEach-Object { $_.Substring($prefix.Length, $_.Length - $prefix.Length)  }
+    | Set-Clipboard
 }
 
 function mklink() {
     cmd /c mklink $args
+}
+
+function touch($fileName) {
+    fsutil file createnew $fileName 0
 }
