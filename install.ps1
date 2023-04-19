b23a22b0cf8dc82dac52fd88c38c0c65f27b4f36
William Barbosa <heytherewill@gmail.com>
2023-04-19 08:08:51 +0200

    🐈 Catppuccin

--- install.ps1
+++ install.ps1
@@ -45,3 +45,22 @@ if ($IsMacOS) {
     brew install --cask font-FiraCode-nerd-font
     brew install ripgrep tokei gh micro fzf bat neofetch tre-command zoxide grex openjdk@11 jandedobbeleer/oh-my-posh/oh-my-posh
 }
+
+Write-Host ""
+Write-Host "Installation finished." -ForegroundColor Green
+Write-Host "Now click these links and install the relevant Catppuccin Macchiato themes" -ForegroundColor Green
+Write-Host "https://github.com/catppuccin/jetbrains" -ForegroundColor Blue
+Write-Host "https://github.com/catppuccin/vscode-pack" -ForegroundColor Blue
+Write-Host "https://github.com/catppuccin/firefox" -ForegroundColor Blue
+Write-Host "https://github.com/catppuccin/duckduckgo" -ForegroundColor Blue
+Write-Host "https://github.com/catppuccin/slack" -ForegroundColor Blue
+Write-Host "https://github.com/catppuccin/telegram" -ForegroundColor Blue
+
+if ($IsWindows) {
+    Write-Host "https://github.com/catppuccin/visual-studio" -ForegroundColor Blue
+    Write-Host "https://github.com/catppuccin/windows-terminal" -ForegroundColor Blue
+}
+
+if ($IsMacOS) {
+    Write-Host "https://github.com/catppuccin/iterm" -ForegroundColor Blue
+}
