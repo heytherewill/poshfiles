ae9092b7f2e88bdef1d445cbdcbcedef98fa8853
William Barbosa <wsantosbarbosa@spotify.com>
2023-08-24 05:42:17 +0200

    🔥 Cleanup macOS java handling

--- macos.ps1
+++ macos.ps1
@@ -6,17 +6,3 @@ if ($IsM1) {
 # Needed for CocoaPods
 $env:LANG="en_US.UTF-8"
 $env:LC_ALL="en_US.UTF-8"
-
-$env:JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
-$env:JAVA_11_HOME=$(/usr/libexec/java_home -v11)
-$env:JAVA_18_HOME=$(/usr/libexec/java_home -v11)
-
-function java11() {
-    $env:JAVA_HOME = $env:JAVA_11_HOME
-}
-
-function java18() {
-    $env:JAVA_HOME = $env:JAVA_18_HOME
-}
-
-$env:JAVA_HOME=$env:JAVA_11_HOME
